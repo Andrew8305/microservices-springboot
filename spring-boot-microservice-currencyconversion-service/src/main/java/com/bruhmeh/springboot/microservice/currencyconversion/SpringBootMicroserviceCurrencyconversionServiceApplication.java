a148e258ca93fa4db6f913c87312fca1f8786f1c
Bruno Delgado <bruno.sdelgado@outlook.com>
2018-05-28 16:04:14 -0300

    feat(currencyExchange): Created CurrencyExchange proxy

--- spring-boot-microservice-currencyconversion-service/src/main/java/com/bruhmeh/springboot/microservice/currencyconversion/SpringBootMicroserviceCurrencyconversionServiceApplication.java
+++ spring-boot-microservice-currencyconversion-service/src/main/java/com/bruhmeh/springboot/microservice/currencyconversion/SpringBootMicroserviceCurrencyconversionServiceApplication.java
@@ -2,8 +2,12 @@ package com.bruhmeh.springboot.microservice.currencyconversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.bruhmeh.springboot.microservice.currencyconversion.proxy")
+@EnableDiscoveryClient
 public class SpringBootMicroserviceCurrencyconversionServiceApplication {
 
 	public static void main(String[] args) {
