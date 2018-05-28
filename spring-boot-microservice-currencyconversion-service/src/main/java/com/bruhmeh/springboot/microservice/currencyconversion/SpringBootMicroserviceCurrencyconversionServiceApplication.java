b5ef993bfe7cdcb235fad521c809dfbf08bb208b
Bruno Delgado <bruno.sdelgado@outlook.com>
2018-05-28 15:20:07 -0300

    feat(currencyExchange): Created CurrencyExchange microservice structure

---
+++ spring-boot-microservice-currencyconversion-service/src/main/java/com/bruhmeh/springboot/microservice/currencyconversion/SpringBootMicroserviceCurrencyconversionServiceApplication.java
@@ -0,0 +1,12 @@
+package com.bruhmeh.springboot.microservice.currencyconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceCurrencyconversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceCurrencyconversionServiceApplication.class, args);
+	}
+}
---
+++ spring-boot-microservice-currencyconversion-service/src/test/java/com/bruhmeh/springboot/microservice/currencyconversion/SpringBootMicroserviceCurrencyconversionServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bruhmeh.springboot.microservice.currencyconversion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMicroserviceCurrencyconversionServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
