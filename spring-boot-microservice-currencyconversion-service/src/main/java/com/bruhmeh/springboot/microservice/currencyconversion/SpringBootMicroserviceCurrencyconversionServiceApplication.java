5b30c4066f67f45b85cc7781664eab5201e2baf3
Bruno Delgado <bruno.sdelgado@outlook.com>
2018-06-03 23:39:55 -0300

    feat(currencyExchange): Hystrix Enabled for the project

--- spring-boot-microservice-currencyconversion-service/src/main/java/com/bruhmeh/springboot/microservice/currencyconversion/SpringBootMicroserviceCurrencyconversionServiceApplication.java
+++ spring-boot-microservice-currencyconversion-service/src/main/java/com/bruhmeh/springboot/microservice/currencyconversion/SpringBootMicroserviceCurrencyconversionServiceApplication.java
@@ -3,11 +3,13 @@ package com.bruhmeh.springboot.microservice.currencyconversion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.bruhmeh.springboot.microservice.currencyconversion.proxy")
 @EnableDiscoveryClient
+@EnableHystrix
 public class SpringBootMicroserviceCurrencyconversionServiceApplication {
 
 	public static void main(String[] args) {
--- spring-boot-microservice-currencyconversion-service/src/main/java/com/bruhmeh/springboot/microservice/currencyconversion/controller/CurrencyConversionController.java
+++ spring-boot-microservice-currencyconversion-service/src/main/java/com/bruhmeh/springboot/microservice/currencyconversion/controller/CurrencyConversionController.java
@@ -1,20 +1,17 @@
 package com.bruhmeh.springboot.microservice.currencyconversion.controller;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.bruhmeh.springboot.microservice.currencyconversion.dto.CurrencyConversionBean;
 import com.bruhmeh.springboot.microservice.currencyconversion.proxy.CurrencyExchangeServiceProxy;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class CurrencyConversionController {
@@ -40,6 +37,7 @@ public class CurrencyConversionController {
 //	}
 	
 	  @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+	  @HystrixCommand
 	  public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
 	      @PathVariable BigDecimal quantity) {
 
