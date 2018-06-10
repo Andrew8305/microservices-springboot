714587b1e7db3d960208248acf4968835a29d5b3
Bruno Delgado <bruno.sdelgado@outlook.com>
2018-06-09 23:38:52 -0300

    feat(auth): Auth server created

---
+++ spring-boot-microservice-auth-server/src/main/java/com/bruhmeh/springboot/microservice/auth/SpringBootMicroserviceAuthServerApplication.java
@@ -0,0 +1,12 @@
+package com.bruhmeh.springboot.microservice.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceAuthServerApplication.class, args);
+	}
+}
